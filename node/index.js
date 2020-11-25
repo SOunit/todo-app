2a55ce65459be2b0986a9cab5030afd23d2d58c9
SOunit <workbookwork7@gmail.com>
2020-11-26 08:18:39 +0900

    added mongodb access and hot reload to React

--- node/index.js
+++ node/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const User = require('./models/user');
+
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send('[/]from node!');
+app.get('/name', (req, res, next) => {
+  res.send({ name: 'Name from node!' });
 });
 
-app.get('/node', (req, res, next) => {
-  res.send('[/node]from node!');
+app.get('/mongo/name', (req, res, next) => {
+  console.log('[/mongo/name]');
+  const joe = new User({ name: 'Name from mongo!' });
+  joe.save().then(() => {
+    console.log('[save done]');
+    User.findOne({ _id: joe._id }).then((user) => {
+      console.log(user);
+      res.send(user);
+    });
+  });
 });
 
 app.listen(5000);
