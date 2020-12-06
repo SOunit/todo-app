33fcefe37ec1a4de3def832a4f86c9302a67d099
SOunit <workbookwork7@gmail.com>
2020-12-06 19:30:42 +0900

    added card, card list

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
+import CardList from './components/CardList/cardList';
 
 function App() {
   return (
     <div>
       <Header />
+      <CardList />
       <Footer />
     </div>
   );
